eada5565cd1ef858db0d2d4fc7e57a2e405a0cf6
BA201 <BA201@MCBA201-C17.classrooms.stlcc>
2017-02-15 13:07:46 -0600

    First Commit

---
+++ src/consoleUI/Console.java
@@ -0,0 +1,9 @@
+
+package consoleUI;
+
+
+public class Console {
+    public static void main(String[] args){
+        System.out.println("Hello Github");
+    }
+}
---
+++ src/simplegithubrepo/Simplegithubrepo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplegithubrepo;
+
+/**
+ *
+ * @author BA201
+ */
+public class Simplegithubrepo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
